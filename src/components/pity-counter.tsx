191a1a72466cb81bead8b9c875c004e307826189
MauriceNino <mauriceprivat98@gmail.com>
2022-10-08 18:27:47 +0200

    feat: add history and settings

--- src/components/pity-counter.tsx
+++ src/components/pity-counter.tsx
@@ -1,6 +1,8 @@
 import {
+  ActionIcon,
   Button,
   Group,
+  Menu,
   Select,
   Stack,
   Switch,
@@ -8,6 +10,7 @@ import {
   Text,
   useMantineTheme,
 } from "@mantine/core";
+import { IconArrowBackUp, IconBrandGithub, IconSettings } from "@tabler/icons";
 import { FC, useState } from "react";
 import {
   DROPS_NAMES,
@@ -60,35 +63,64 @@ export const PityCounter: FC = () => {
 
   return (
     <Stack>
-      <Group sx={{ alignItems: "flex-end" }}>
-        <Select
-          label="Stage"
-          value={selectedStage}
-          onChange={(v) => setSelectedStage(v as JOStages)}
-          data={Object.entries(JOINT_OPS_NAMES).map(([stage, name]) => ({
-            value: stage,
-            label: name,
-          }))}
-          sx={{
-            width: 220,
-          }}
-        />
-
-        <Group>
-          <Button
-            onClick={() =>
-              dispatch(
-                stateActions.openChest({ stage: selectedStage, chipEnabled })
-              )
-            }
-          >
-            Chest opened
-          </Button>
-          <Switch
-            label="JO-Chip enabled"
-            checked={chipEnabled}
-            onChange={(e) => setChipEnabled(e.currentTarget.checked)}
+      <Group position="apart" sx={{ flexWrap: "nowrap" }}>
+        <Group sx={{ alignItems: "flex-end" }}>
+          <Select
+            value={selectedStage}
+            onChange={(v) => setSelectedStage(v as JOStages)}
+            data={Object.entries(JOINT_OPS_NAMES).map(([stage, name]) => ({
+              value: stage,
+              label: name,
+            }))}
+            sx={{
+              width: 220,
+            }}
           />
+
+          <Group>
+            <Button
+              onClick={() =>
+                dispatch(
+                  stateActions.openChest({ stage: selectedStage, chipEnabled })
+                )
+              }
+            >
+              Chest opened
+            </Button>
+            <Switch
+              label="JO-Chip enabled"
+              checked={chipEnabled}
+              onChange={(e) => setChipEnabled(e.currentTarget.checked)}
+            />
+          </Group>
+        </Group>
+
+        <Group sx={{ flexWrap: "nowrap", alignSelf: "flex-start" }}>
+          <ActionIcon
+            size="lg"
+            variant="transparent"
+            onClick={() => dispatch(stateActions.goBackHistory())}
+          >
+            <IconArrowBackUp size={26} />
+          </ActionIcon>
+
+          <Menu shadow="md" position="bottom-end">
+            <Menu.Target>
+              <ActionIcon size="lg" variant="outline">
+                <IconSettings size={26} />
+              </ActionIcon>
+            </Menu.Target>
+            <Menu.Dropdown>
+              <Menu.Item
+                icon={<IconBrandGithub size={14} />}
+                component="a"
+                href="https://github.com/MauriceNino/tof-pity"
+                target="_blank"
+              >
+                GitHub Project
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
         </Group>
       </Group>
 
--- src/redux/stateSlice.ts
+++ src/redux/stateSlice.ts
@@ -14,6 +14,20 @@ export enum HistoryChangeType {
   ITEM_DROP = "item_drop",
 }
 
+type HistoryChestOpen = {
+  type: HistoryChangeType.CHEST_OPEN;
+  stage: JOStages;
+  withChip: boolean;
+};
+type HistoryItemDrop = {
+  type: HistoryChangeType.ITEM_DROP;
+  stage: JOStages;
+  item: GearTypes | MatrixTypes;
+  pity: number;
+};
+
+type History = HistoryChestOpen | HistoryItemDrop;
+
 type State = {
   joCounts: Partial<
     Record<
@@ -23,17 +37,15 @@ type State = {
       }
     >
   >;
-  changeHistory: (
-    | {
-        type: HistoryChangeType.CHEST_OPEN;
-        withChip: boolean;
-      }
-    | {
-        type: HistoryChangeType.ITEM_DROP;
-        item: GearTypes | MatrixTypes;
-      }
-  )[];
+  changeHistory: History[];
 };
+
+const historyIsChestOpen = (history: History): history is HistoryChestOpen =>
+  history.type === HistoryChangeType.CHEST_OPEN;
+
+const historyIsItemDrop = (history: History): history is HistoryItemDrop =>
+  history.type === HistoryChangeType.ITEM_DROP;
+
 const initialState: State = {
   joCounts: {},
   changeHistory: [],
@@ -43,6 +55,37 @@ const getPity = (type: JODrops, chipEnabled: boolean) => {
   const behavior = SUPPLY_CHIP_BEHAVIOR[type];
 
   return chipEnabled ? behavior.withChip : behavior.withoutChip;
+};
+
+const addToAll = (
+  stage: PerItemCount,
+  chests: number,
+  chipEnabled: boolean
+) => {
+  stage[GearTypes.Gold].currentPity =
+    stage[GearTypes.Gold].currentPity +
+    chests * getPity(GearTypes.Gold, chipEnabled);
+  stage[GearTypes.Purple].currentPity =
+    stage[GearTypes.Purple].currentPity +
+    chests * getPity(GearTypes.Purple, chipEnabled);
+  stage[GearTypes.Blue].currentPity =
+    stage[GearTypes.Blue].currentPity +
+    chests * getPity(GearTypes.Blue, chipEnabled);
+  stage[GearTypes.Green].currentPity =
+    stage[GearTypes.Green].currentPity +
+    chests * getPity(GearTypes.Green, chipEnabled);
+  stage[MatrixTypes.Gold].currentPity =
+    stage[MatrixTypes.Gold].currentPity +
+    chests * getPity(MatrixTypes.Gold, chipEnabled);
+  stage[MatrixTypes.Purple].currentPity =
+    stage[MatrixTypes.Purple].currentPity +
+    chests * getPity(MatrixTypes.Purple, chipEnabled);
+  stage[MatrixTypes.Blue].currentPity =
+    stage[MatrixTypes.Blue].currentPity +
+    chests * getPity(MatrixTypes.Blue, chipEnabled);
+  stage[MatrixTypes.Green].currentPity =
+    stage[MatrixTypes.Green].currentPity +
+    chests * getPity(MatrixTypes.Green, chipEnabled);
 };
 
 const types = [
@@ -66,16 +109,7 @@ export const stateSlice = createSlice({
     ) => {
       const stage = state.joCounts[action.payload.stage];
       if (stage != null) {
-        stage.counts = Object.fromEntries(
-          types.map((t) => [
-            t,
-            {
-              currentPity:
-                stage.counts[t].currentPity +
-                getPity(t, action.payload.chipEnabled),
-            },
-          ])
-        ) as PerItemCount;
+        addToAll(stage.counts, 1, action.payload.chipEnabled);
       } else {
         state.joCounts[action.payload.stage] = {
           counts: Object.fromEntries(
@@ -89,6 +123,7 @@ export const stateSlice = createSlice({
 
       state.changeHistory.push({
         type: HistoryChangeType.CHEST_OPEN,
+        stage: action.payload.stage,
         withChip: action.payload.chipEnabled,
       });
     },
@@ -98,12 +133,27 @@ export const stateSlice = createSlice({
     ) => {
       const stage = state.joCounts[action.payload.stage];
       if (stage) {
-        stage.counts[action.payload.type].currentPity = 0;
-
         state.changeHistory.push({
           type: HistoryChangeType.ITEM_DROP,
+          stage: action.payload.stage,
           item: action.payload.type,
+          pity: stage.counts[action.payload.type].currentPity,
         });
+
+        stage.counts[action.payload.type].currentPity = 0;
+      }
+    },
+    goBackHistory: (state) => {
+      const lastHistory = state.changeHistory.pop();
+      if (lastHistory) {
+        const stage = state.joCounts[lastHistory.stage];
+
+        if (historyIsChestOpen(lastHistory) && stage) {
+          addToAll(stage.counts, -1, lastHistory.withChip);
+        }
+        if (historyIsItemDrop(lastHistory) && stage) {
+          stage.counts[lastHistory.item].currentPity = lastHistory.pity;
+        }
       }
     },
   },
